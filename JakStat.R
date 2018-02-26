c53e7caebe00960a8aaf8d110b5cc964c8dcdebd
Newmi1988 <newmithegreat@hotmail.de>
2018-02-26 18:48:26 +0100

    skelleton of the r package

---
+++ JakStat.R
@@ -0,0 +1,86 @@
+graphics.off()
+rm(list=ls())
+
+library('deSolve')
+
+## transform
+N = 10^0.31
+a = 0.00
+x0 = c(N, 0, 0, 0)
+# y <- c(logX = log(x0))
+y <- c(X = x0)
+times <- c( 0.0208,  0.1098,   0.2696,    0.4999,    0.8002,    1.1697,    1.6077,    2.1129,    2.6843,    3.3205,    4.0200,    4.7811,    5.6020,    6.4808,    7.4154,    8.4035,    9.4429,   10.5310, 11.6653,   12.8431,   14.0616,   15.3179,   16.6090,   17.9319,   19.2834,   20.6603,   22.0594,   23.4773,   24.9107,   26.3561,   27.8102,   29.2695,   30.7305,   32.1898,   33.6439,   35.0893, 36.5227,   37.9406,   39.3397,   40.7166,   42.0681,   43.3910,   44.6821,   45.9384,   47.1569,   48.3347,   49.4690,   50.5571,   51.5965,   52.5846,   53.5192,   54.3980,   55.2189,   55.9800, 56.6795,   57.3157,   57.8871,   58.3923,   58.8303,   59.1998,   59.5001,   59.7304,   59.8902,   59.9792)
+#times <- seq(from=0, to=60, length.out = 100)
+parameters = 10^c(0.31, -1, -0.49, 0.42, -0.21, -0.34)
+
+inputData <- read.table('DATA1_hall_inp.txt')
+colnames(inputData) <- c('t','u')
+measure <- read.table("DATA1_hall.txt")
+colnames(measure) <- c('t','y1', 'y1sd', 'y2', 'y2sd')
+
+inputSys <- list()
+inputSys$u <- approxfun(x = inputData$t, y = inputData$u, method = 'linear', rule = 2)
+
+modelJakStat  <- function(t, x, parameters, input) {
+  with (as.list(parameters),{
+
+    k1 = parameters[1]
+    k2 = parameters[2]
+    k3 = parameters[3]
+    k4 = parameters[4]
+    s1 = parameters[5]
+    s2 = parameters[6]
+
+    u <- input$u(t)
+
+    dx1 = -k1 * x[1]  * u
+    dx2 = k1 *  x[1]  * u - k2 * x[2]^2
+    dx3 = -k3*x[3] + 0.5*k2*x[2]^2
+    dx4 = k3 * x[3]
+
+    list(c(dx1 ,dx2 ,dx3 ,dx4 ))
+  })
+}
+
+x <- as.data.frame(ode(y = y, times = times, parms = parameters, func = modelJakStat, input = inputSys, atol = 1e-10))
+
+matplot(x$time,x[,-1], type = 'l')
+
+measJakStat <- function(x) {
+
+  s1 <- 10^(-0.21)
+  s2 <- 10^(-0.34)
+
+  y1 = s1*(x[,2]+ 2*x[,3])
+  y2 = s2*(x[,1] + x[,2] + 2*x[,3])
+
+  return(list(y1,y2))
+}
+
+y = measJakStat(x[,-1])
+
+
+y1 = approx(x = x[,1], y=y[[1]] ,xout = measure[,1])
+y2 = approx(x = x[,1], y=y[[2]] ,xout = measure[,1])
+
+results <- data.frame(measure, yhat1 = y1$y, yhat2 = y2$y)
+
+library('ggplot2')
+p <- ggplot(results, aes(x = t)) + geom_line(aes(y = y1), colour='red') + geom_line(aes(y = yhat1), colour = "blue")
+p + geom_errorbar(aes(ymin = y1-y1sd, ymax=y1+y1sd))
+
+q <- ggplot(results, aes(x = t)) + geom_line(aes(y = y2), colour='red') + geom_line(aes(y = yhat2), colour = "blue")
+q + geom_errorbar(aes(ymin = y2-y2sd, ymax=y2+y2sd))
+
+## needed objects
+optW <- rep(1,4)
+y <- data.frame(measure$t, measure$y1, measure$y2)
+sd <- data.frame(results$y1sd, results$y2sd)
+
+
+
+greedyApproach(alphaStep = 0.01, alpha2 = 0.1,
+               x0 = x0, optW = c(1,1,1,1) , times=times,
+               measFunc= measJakStat,  measData = y, std = sd,
+               parameters = parameters, systemInput = inputData,
+               modelFunc = modelJakStat, plotEstimates = T, conjGrad = F)
