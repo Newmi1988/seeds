7b8f6d86f264037c898b523b3e3190653415fdf6
Newmi1988 <newmi1988@gmail.com>
2018-03-12 13:06:10 +0100

    updated the help files

--- R/seeds.R
+++ R/seeds.R
@@ -1,2 +1,16 @@
 #' @details Details
+#' 
+#' The conjugate gradient based algorithm uses a greedy algorithm to estimate a 
+#' sparse controll that tries to minimize the discrepancies between a given 
+#' 'nominal model given the measurements (e.g from an experiment). The algorithm
+#' uses \pkg{deSolve} to calculate the hidden inputs w based on the adjoint equations
+#' of the ODE-System. 
+#' 
+#' The adjoint equations are calculated using \pkg{Deriv}. For the usage of the 
+#' algorithm please look into the examples and documentation given for the
+#' functions 
+#' \describe{
+#'    \item{\code{\link{greedyApproach}}}{a greedy algorithm to calculate a sparse control}
+#'    \item{\code{\link{dynElasticNet}}}{a conjugate gradient optimal control algorithm with L2 regulation} 
+#' }
 "_PACKAGE"
