1929564d319f6a8c71843ccfd5f7b7608f1ff0c8
Newmi1988 <newmi1988@gmail.com>
2018-03-12 12:41:23 +0100

    added more help files

--- R/seeds.R
+++ R/seeds.R
@@ -1,12 +1,2 @@
-#' @details 
-#' 
-#' Algorithms for calculating the hidden inputs of a system
-#' 
-#' SEEDS combines two algorithms to estimate the hidden inputs of a nominal system
-#' with given measurements.
-#' 
-#' This package combines two algorithms for calculating the hidden inputs. A more
-#' simple approach using optimal control based on the maximum principal using a
-#' conjugate gradient algorithm and a MCMC-method.
-#' 
+#' @details Details
 "_PACKAGE"
