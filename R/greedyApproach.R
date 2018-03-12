0e2b82efe876c365388b3c3b42281a616ef8d51e
Newmi1988 <newmi1988@gmail.com>
2018-03-12 12:08:39 +0100

    #1

--- R/greedyApproach.R
+++ R/greedyApproach.R
@@ -1,3 +1,15 @@
+#' Algorithms for calculating the hidden inputs of a system
+#' 
+#' SEEDS combines two algorithms to estimate the hidden inputs of a nominal system
+#' with given measurements.
+#' 
+#' This package combines two algorithms for calculating the hidden inputs. A more
+#' simple approach using optimal control based on the maximum principal using a
+#' conjugate gradient algorithm and a MCMC-method.
+#' 
+"_PACKAGE"
+
+
 #' Greedy Approach Algorithm
 #'
 #' calculates controls based on a first optimisation with gradient descent; should result in a sparse vector
