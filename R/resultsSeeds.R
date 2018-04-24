e3fefe35498a7de14c84f6bd865dc6a422df988f
Newmi1988 <newmi1988@gmail.com>
2018-04-24 15:32:42 +0200

    changes to the plot function - new design that resembles the paper - made annotating the plot possible

--- R/resultsSeeds.R
+++ R/resultsSeeds.R
@@ -51,37 +51,71 @@ plotResultsSeeds  <- function(x,y) {
   
   seedsobj = x
   
+  if(!missing(y)){
+    annoX <- y[[1]]
+    annoY <- y[[2]]
+  } else {
+    annoX <- names(seedsobj@stateEstimates[,-1])
+    annoY <- names(seedsobj@outputEstimates[,-1])
+  }
+  
+  labelX <- function(ls,val) {
+    dic <- list(annoX)
+    return(dic[val])
+  }
+  
+  labelY <- function(ls,val) {
+    dic <- list(annoY)
+    return(dic[val])
+  }
+
+  
   # added formating for plotting the states in the right order
-  reformatOrder <- function(df){
+  reformatOrder <- function(df,annoT){
     df$facet = factor(df$state, levels = as.character(unique(factor(df$state))))
     
     return(df)
   }
   
-  plot1 <- ggplot2::ggplot(reformatOrder(tidyr::gather(seedsobj@stateEstimates,state, value, -1)), ggplot2::aes(x=t, y=value, colour="red"))+ 
+  plot1 <- ggplot2::ggplot(reformatOrder(tidyr::gather(seedsobj@stateEstimates,state, value, -1)), ggplot2::aes(x=t, y=value, colour='red'))+ 
     ggplot2::geom_line()+
-    ggplot2::geom_line(data = reformatOrder(tidyr::gather(seedsobj@stateNominal,state, value, -1)), ggplot2::aes(x=t, y=value, colour="blue"))+ 
+    ggplot2::geom_line(data = reformatOrder(tidyr::gather(seedsobj@stateNominal,state, value, -1)), ggplot2::aes(x=t, y=value, colour='blue'))+ 
     ggplot2::geom_ribbon(data=reformatOrder(dplyr::inner_join(tidyr::gather(seedsobj@stateUnscertainUpper, state, value, -1), tidyr::gather(seedsobj@stateUnscertainLower, state, value, -1), by = c("t","state"))), ggplot2::aes(x= t, ymin = value.y, ymax=value.x), alpha=0.2, inherit.aes = FALSE)+
-    ggplot2::theme(legend.position="none")+
-    ggplot2::facet_wrap(~facet)
+    ggplot2::labs(x= 't', y='value',color = "states")+
+    ggplot2::scale_color_manual(breaks= c("red","blue"), labels = c("estimate","nominal"), values = c("blue","red"))+
+    ggplot2::theme(strip.background = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank())+
+    ggplot2::facet_wrap(~facet, labeller = labelX)
 
   
   
   plot2 <- ggplot2::ggplot(data=reformatOrder(tidyr::gather(seedsobj@hiddenInputEstimates,state,value,-1)), ggplot2::aes(x=t,y=value, colour="red"))+
     ggplot2::geom_line()+
     ggplot2::geom_ribbon(data=reformatOrder(dplyr::inner_join(tidyr::gather(seedsobj@hiddenInputUncertainUpper, state, value, -1), tidyr::gather(seedsobj@hiddenInputUncertainLower, state, value, -1), by = c("t","state"))), ggplot2::aes(x= t, ymin = value.y, ymax=value.x), alpha=0.2, inherit.aes = FALSE)+
-    ggplot2::scale_color_discrete()+
-    ggplot2::theme(legend.position="none")+
+    ggplot2::theme(legend.position = "none",
+                   strip.background = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank())+
     ggplot2::facet_wrap(~facet)
   
-  
-  plot3 <- ggplot2::ggplot(data=reformatOrder(tidyr::gather(seedsobj@outputEstimates,state,value, -1)), ggplot2::aes(x=t, y=value, colour="red"))+
+  plot3 <- ggplot2::ggplot(data=reformatOrder(tidyr::gather(seedsobj@outputEstimates,state,value, -1)), ggplot2::aes(x=t, y=value, colour=state))+
     ggplot2::geom_line()+
     ggplot2::geom_errorbar(data=reformatOrder(dplyr::inner_join(tidyr::gather(seedsobj@Data, state,value, -1), tidyr::gather(seedsobj@DataError,state,value, -1), by=c("t","state"))), ggplot2::aes(x=t, ymin= value.x - value.y , ymax = value.x + value.y), inherit.aes = FALSE)+
     ggplot2::geom_point(data =reformatOrder(tidyr::gather(seedsobj@Data,state,value,-1)), ggplot2::aes(x=t, y=value), colour="black") +
-    ggplot2::scale_color_discrete()+
-    ggplot2::theme(legend.position="none")+
-    ggplot2::facet_wrap(~facet)
+    ggplot2::labs(x= 't', y='value',color = "Estimated \n measurements")+
+    ggplot2::scale_color_manual(labels = labels(seedsobj@outputEstimates[,-1])[[2]], values = rep("red",length(labels(seedsobj@outputEstimates[,-1])[[2]])))+
+    ggplot2::theme(legend.position = "none",
+                  strip.background = ggplot2::element_blank(),
+                  panel.background = ggplot2::element_blank(),
+                  panel.border = ggplot2::element_blank(),
+                  panel.grid.major = ggplot2::element_blank(),
+                  panel.grid.minor = ggplot2::element_blank())+
+    ggplot2::facet_wrap(~facet, labeller = labelY)
   
   return(list(plot1,plot2, plot3))
   
@@ -105,6 +139,32 @@ setMethod(f = "plot",
           signature = c(x="resultsSeeds",y="missing"),
           definition = function(x,y)
           {
+            plotList <- plotResultsSeeds(x,y)
+            
+            return(plotList)
+          }
+)
+
+
+ 
+setGeneric(name = "plotAnno", function(x,stateAnno,measAnno) standardGeneric("plotAnno"))
+
+#' Create annotated plot
+#' 
+#' Create a annotated plot with given state and measurement names. The plots are
+#' equal to the output of the normal plot function.
+#' @param x an object of type resultsSeeds which contains the results of the algorithms
+#' @param stateAnno a character vector describing the names of the states
+#' @param measAnno a character vector describing the names of the measurements
+#' 
+#' @rdname resultsSeeds-methods
+#' 
+#' @export
+setMethod(f = "plotAnno",
+          signature = "resultsSeeds",
+          definition = function(x,stateAnno,measAnno)
+          {
+            y <- list(stateAnno,measAnno)
             plotList <- plotResultsSeeds(x,y)
             
             return(plotList)
--- examples/JakStat.R
+++ examples/JakStat.R
@@ -57,3 +57,10 @@ results <- greedyApproach(alphaStep = 0.01, alpha2 = 0.4, Beta = 0.8, epsilon = 
                modelFunc = modelJakStat, plotEstimates = TRUE, conjGrad = FALSE, cString = JakStatConst))
 
 
+plot(results)
+
+statesAnno <- c("STAT5 cyt.", "STAT5p cyt.", "STAT5p-d cyt.", "stat5-d nucl")
+measurAnno <- c("total STAT5p", "total STAT5")
+
+plotAnno(results,stateAnno = statesAnno, measAnno =  measurAnno)
+
