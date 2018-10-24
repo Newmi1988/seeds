358d52979217014f33d01a516a8b25cb6fe52424
Newmi1988 <newmi1988@gmail.com>
2018-10-24 19:26:35 +0200

    demonstration of the sbml model loading

---
+++ examples/uvb_sbml.R
@@ -0,0 +1,11 @@
+modelStr <- 'BIOMD0000000545_url.xml'
+uvb <- importSBML(modelStr)
+
+y <- uvbData[,1:3]
+uvb <- setMeas(theObject = uvb, y)
+
+# Plot the nominal solution
+# plot(nominalSol(odeModel = uvb))
+
+res <- greedyApproach(odeModel = uvb, alphaStep = 201, alpha2 = 0.0001, plotEstimates = TRUE)
+plot(res)
