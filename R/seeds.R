94bf2f0148171fa6164d6aca624dc3923b69e980
Tobias Newmiwaka <newmi1988@gmail.com>
2020-01-25 16:45:24 +0100

    changed describtion package

--- R/seeds.R
+++ R/seeds.R
@@ -1,6 +1,6 @@
 #' @details Details
 #' 
-#' The algorithm calculates the needed equations using the \code{\link{Deriv}}
+#' The first algorithm (SGDN) calculates the needed equations using the \code{\link{Deriv}}
 #' function of the \pkg{Deriv} package. The process is implemented through the use
 #' of the S4 class \code{\link{odeEquations-class}}. 
 #' 
@@ -12,12 +12,23 @@
 #' 
 #' The adjoint equations are calculated using the \code{\link{ode}} function of the 
 #' \pkg{deSolve} package. For the usage of the algorithm please look into the 
-#' examples and documentation given for the functions 
+#' examples and documentation given for the functions.
+#' 
+#' The second algorithm is called Bayesian Dynamic Elastic Net (BDEN).
+#  The BDEN as a new and fully probabilistic approach, supports the modeller in an 
+#' algorithmic manner to identify possible sources of errors in ODE based models on 
+#' the basis of experimental data.  THE BDEN does not require pre-specified hyper-parameters. 
+#' BDEN thus provides a systematic Bayesian computational method to identify target nodes and 
+#' reconstruct the corresponding error signal including detection of missing and 
+#' wrong molecular interactions within the assumed model. The method works for ODE 
+#' based systems even with uncertain knowledge and noisy data. 
+#' 
 #' \describe{
-#'    \item{\code{\link{sgdn}}}{a greedy algorithm to calculate a sparse control}
+#'    \item{\code{\link[seeds]{sgdn}}}{a greedy algorithm to calculate a sparse control}
+#'    \item{\code{\link[seeds]{BDEN}}}{a basian mcmc approach}
 #' }
 #' 
-#' @references \strong{Benjamin Engelhardt, Holger Fröhlich, Maik Kschischo} 
+#' @references \strong{Benjamin Engelhardt, Holger Froehlich, Maik Kschischo} 
 #' Learning (from) the errors of a systems biology model, \emph{Nature Scientific Reports},
 #'  6, 20772, 2016 \url{https://www.nature.com/articles/srep20772}
 "_PACKAGE"
